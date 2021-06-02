8b9c23f10f6818f0916ede2c44200dd3be1ab82c
franciscokataldocl <franciscolork@gmail.com>
2021-06-01 23:23:21 -0400

    proyecto terminado

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+//smoth scroll
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+//tool tip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+  
